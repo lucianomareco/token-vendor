23ef8fbd973ba2339e37518d316e7785e3f64503
luciano <lucianogastonmareco@gmail.com>
2024-06-05 20:21:48 -0300

    feat: add sc logic

--- packages/hardhat/contracts/Vendor.sol
+++ packages/hardhat/contracts/Vendor.sol
@@ -1,13 +1,16 @@
 pragma solidity 0.8.4; //Do not change the solidity version as it negativly impacts submission grading
 // SPDX-License-Identifier: MIT
 
-// import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 import "./YourToken.sol";
 
-contract Vendor {
-  // event BuyTokens(address buyer, uint256 amountOfETH, uint256 amountOfTokens);
+contract Vendor is Ownable {
+  event BuyTokens(address buyer, uint256 amountOfETH, uint256 amountOfTokens);
+  event SellTokens(address seller, uint256 amountOfTokens, uint256 amountOfETH);
 
   YourToken public yourToken;
+
+  uint256 public constant tokensPerEth = 100;
 
   constructor(address tokenAddress) {
     yourToken = YourToken(tokenAddress);
@@ -15,7 +18,22 @@ contract Vendor {
 
   // ToDo: create a payable buyTokens() function:
 
+  function buyTokens() public payable {
+    uint256 amount = msg.value * tokensPerEth;
+    yourToken.transfer(msg.sender, amount);
+    emit BuyTokens(msg.sender, msg.value, amount);
+  }
+
   // ToDo: create a withdraw() function that lets the owner withdraw ETH
 
+  function withdraw() public onlyOwner {
+    payable(owner()).transfer(address(this).balance);
+  }
+
   // ToDo: create a sellTokens(uint256 _amount) function:
+  function sellTokens(uint256 _amount) public {
+    yourToken.transferFrom(msg.sender, address(this), _amount);
+    payable(msg.sender).transfer(_amount / tokensPerEth);
+    emit SellTokens(msg.sender, _amount, _amount / tokensPerEth);
+  }
 }
